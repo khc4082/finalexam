ca76c10cfde2ca58cd2a3b73867478373482c7d1
khc4082 <80371590+khc4082@users.noreply.github.com>
2021-06-09 04:29:49 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.hyeonseong;
+
+public class Main {
+
+    public static void main(String[] args) {
+     
+            // 000000
+            // 010000
+
+            SimulatedAnnealing sa = new SimulatedAnnealing(1, 0.95, 100);
+            sa.solve(new Problem() {
+                @Override
+                public double fit(double x) {
+                    return 0.16*x*x*x*x -x*x + 0.37*x + 5;
+                }
+
+                @Override
+                public boolean isNeighborBetter(double f0, double f1) {
+                    return f1 > f0;
+                }
+            }, 0, 31);
+
+            System.out.println(sa.hist);
+            // x=19, f(x)=441
+        }
+    }
